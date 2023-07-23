742e02b36b0b8448ef38fbfdfbce9f6500861df4
tanvi <tanvi9sinha@gmail.com>
2023-07-22 23:59:12 -0400

    Updated work and education

--- Documents/my-portfolio/src/components/Education/Education.js
+++ Documents/my-portfolio/src/components/Education/Education.js
@@ -1,12 +1,47 @@
 import React from "react";
+import ncsu from '../../Assets/ncsu.png';
+import pes from '../../Assets/pes.png';
 
-function UnderConstruction() {
+function Education() {
   return (
-    <div style={{ fontFamily: 'Electrolize, sans-serif', textAlign: 'center', paddingTop: "70px"  }}>
-      <h1>Page Under Construction</h1>
-      <p>This page is currently under construction. Please check back later.</p>
+    <div className="education-section">
+      <section>
+        <div>
+          <img src={ncsu} alt="North Carolina State University" />
+          <h2>
+            North Carolina State University
+            <span className="college-name">Raleigh, NC</span>
+            <span className="years">Aug 2022 - Dec 2023 (Expected)</span>
+          </h2>
+          <p>
+            Major: Computer Science
+            <br />
+            Relevant Coursework: Object Oriented Design & Development, DBMS, Algorithms, Neural Networks, Software Engineering
+            <br />
+            GPA: 4.0 / 4.0
+          </p>
+        </div>
+      </section>
+      <section>
+        <div>
+          <img src={pes} alt="Your College Name" />
+          <h2>
+            PES University
+            <span className="college-name">Bangalore, India</span>
+            <span className="years">Aug 2016 - May 2020</span>
+          </h2>
+          <p>
+            Major: Electronics & Communication Engineering
+            <br />
+            Minor: Computer Science Engineering
+            <br />
+            GPA: 9.15 / 10.0
+          </p>
+        </div>
+      </section>
+      {/* Add more sections as needed */}
     </div>
   );
 }
 
-export default UnderConstruction;
+export default Education;
